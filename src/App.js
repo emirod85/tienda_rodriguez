63672fa2add705dcc8b398c847dab79ada813ae0
emirod85 <emilianorodriguez85@hotmail.com>
2021-09-22 14:30:23 -0300

    Desafio C3 - Menu e-commerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+      <NavBar/>
+   
   );
 }
 
 export default App;
+
+
+// import React, { Component, useState } from 'react';
+// import './App.css';
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     /*
+//     Estado
+//     Basicamente inicializar informacion de mi componente
+//     */
+//     this.state = {
+//       name: "Gustavo Adolfo",
+//       age: 32,
+//       hobbies: ["play soccer", "play chess", "dance"]
+//     }
+//   }
+
+//   render() {
+//     return <MyFirstComponent name={this.state.name} age={this.state.age} hobbies={this.state.hobbies} component={newComponent}/>
+//   }
+// }
+
+// const App = () => {
+//   const [name, setName] = useState("Gustavo Adolfo");
+//   const [age, setAge] = useState(32);
+//   const [hobbies, setHobbies] = useState(["play soccer", "play chess", "dance"]);
+
+//   return <MyFirstComponent name={name} age={age} hobbies={hobbies} component={newComponent}/>
+// }
+
+
+// const MyFirstComponent = (props) => {
+//   return (
+//     <h1>{props.name} {props.age} {props.hobbies} {props.component({contentButton: 'Hacer click'})}</h1>
+//   )
+// }
+
+
+// const newComponent = ({ contentButton }) => {
+//   function callAlert() {
+//     alert("Hello World!");
+//   }
+//   return <button type="button" onClick={callAlert}>{contentButton}</button>
+// }
+
+// export default App;
+
+
+
+// const incrementar = () => {
+//   if (contador < stock) {
+//       setContador(contador + 1)
+//   }
+// }  <button onClick={incrementar}> + </button>
+
+
+
---
+++ src/components/NavBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar,NavDropdown,Nav,Form,FormControl,Button} from 'react-bootstrap';
+
+const NavBar = () => {
+    return (
+    
+             
+      <header className="App-header">
+      <Navbar  bg="light" expand="lg">
+  <Navbar.Brand href="#">TiendaEmiRodr</Navbar.Brand>
+  <Navbar.Toggle aria-controls="navbarScroll" />
+  <Navbar.Collapse id="navbarScroll">
+    <Nav
+      className="mr-auto my-2 my-lg-0"
+      style={{ maxHeight: '100px' }}
+      navbarScroll
+    >
+      <Nav.Link href="#action1">Home</Nav.Link>
+      <Nav.Link href="#action2">Latin Stores</Nav.Link>
+      <NavDropdown title="LatinFood" id="navbarScrollingDropdown">
+        <NavDropdown.Item href="#action3">Products</NavDropdown.Item>
+        <NavDropdown.Item href="#action4">Favorites</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action5">SignUp your store</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#" disabled>
+        Link
+      </Nav.Link>
+    </Nav>
+    <Form className="d-flex">
+      <FormControl
+        type="search"
+        placeholder="Search"
+        className="mr-2"
+        aria-label="Search"
+      />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+      </header>
+        
+    )
+}
+
+export default NavBar
